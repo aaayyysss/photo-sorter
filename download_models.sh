4f8ddcf69a413e810968c4c47cc6684cd249fac1
aaayyysss <ammar.suleiman@outlook.de>
2025-08-25 17:34:54 -0700

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -1,18 +1,17 @@
+#!/bin/bash
 set -e
 
 echo "📥 Creating buffalo_l model directory..."
 mkdir -p .insightface/models/buffalo_l
-
 cd .insightface/models/buffalo_l
 
-echo "📥 Downloading model files from Google Drive..."
+echo "📥 Downloading model files from Google Drive using gdown..."
 
-# Replace these links with your actual direct download links
-curl -L -o config.yaml 'https://drive.google.com/file/d/1Ldp7BATcllpe_yGjJzrTBFvHHsgtSpV5/view?usp=drive_link'
-curl -L -o det_10g.onnx 'https://drive.google.com/file/d/1Px5xAjdBzYZjvGGEsZEBm5bDi--1NU6u/view?usp=drive_link'
-curl -L -o genderage.onnx 'https://drive.google.com/file/d/1VreU-_OqA-oeiR9V583Q_LPh_1sld8Yk/view?usp=drive_link'
-curl -L -o w600k_r50.onnx 'https://drive.google.com/file/d/1EbNrHaSieVeb7Hu399WHDDwElTKcEyKD/view?usp=drive_link'
-curl -L -o 1k3d68.onnx 'https://drive.google.com/file/d/1gWLXugVbjXSE57QsVCpBmN_tyJpXlsAc/view?usp=drive_link'
-curl -L -o 2d106det.onnx 'https://drive.google.com/file/d/1XrOeD77P53jFE36Qd41iPECidfcYhWqw/view?usp=drive_link'
+gdown --id 1Ldp7BATcllpe_yGjJzrTBFvHHsgtSpV5 -O config.yaml
+gdown --id 1Px5xAjdBzYZjvGGEsZEBm5bDi--1NU6u -O det_10g.onnx
+gdown --id 1VreU-_OqA-oeiR9V583Q_LPh_1sld8Yk -O genderage.onnx
+gdown --id 1EbNrHaSieVeb7Hu399WHDDwElTKcEyKD -O w600k_r50.onnx
+gdown --id 1gWLXugVbjXSE57QsVCpBmN_tyJpXlsAc -O 1k3d68.onnx
+gdown --id 1XrOeD77P53jFE36Qd41iPECidfcYhWqw -O 2d106det.onnx
 
 echo "✅ All files downloaded successfully."
